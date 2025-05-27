af8bfa731fb46782a3055692b54e344ce2f4512b
Oleksandr Dunaievskyi <o_dunaievskyi@proton.me>
2025-05-27 21:02:29 +0300

    parse pipewire graph to find steam client app name

--- gamerecorder/main.py
+++ gamerecorder/main.py
@@ -7,6 +7,7 @@ import re
 import datetime
 import time
 import glob
+import json
 from pydantic import BaseModel, ValidationError
 import tomllib
 from xdg_base_dirs import xdg_config_home
@@ -32,7 +33,6 @@ log_gsr = colorlog.getLogger('GSR')
 log_gsr.addHandler(handler)
 log_gsr.setLevel(colorlog.DEBUG)
 
-
 class Config(BaseModel):
     """  monitor output. Anything that can be "-w" parameter of gsr.  Example: DP-2  """
     mon: str
@@ -57,68 +57,90 @@ else:
     quit(1)
 
 
-def build_process_list() -> list[dict]:
+class ProcessInfo(NamedTuple):
+    pid: int
+    name: str
+    args: list[str]
+
+class GameInfo(NamedTuple):
+    pid: int
+    title: str
+    pipewire_name: str
+
+def get_all_processes() -> list[ProcessInfo]:
     processes = []
     for x in psutil.process_iter():
         try:
-            processes.append({
-                'pid': x.pid,
-                'name': x.name(),
-                'cmd': x.cmdline()
-            })
+            processes.append(ProcessInfo(pid=x.pid, name=x.name(), args=x.cmdline()))
         except psutil.NoSuchProcess: ...
     return processes
 
-
 def gsr_exec(output_file: str,
-             audio_application_name: Optional[str],
+             pipewire_name: str,
              ) -> subprocess.Popen:
     args = [
         'gpu-screen-recorder',
         '-w', conf.mon,
         '-f', str(conf.fps),
         '-k', 'hevc_10bit',
-        '-a', (f'app:{audio_application_name}' if audio_application_name else 'default_output'),
+        '-a', 'app:'+ pipewire_name,
         '-o', output_file,
         ]
-    log_main.debug('gsr_exec with params: %s', ' '.join(args))
+    log_main.info('gsr_exec with params: %s', ' '.join(args))
     return subprocess.Popen(args=args)
 
 
 def cycle():
-    process_list = build_process_list()
-
     log_main.info('detecting game...')
 
-    class GameMetadata(NamedTuple):
-        title: str
-        exe: str
-        pid: int
-
-    def find_steam_proton_game_executable_by_path_element(path_element: str) -> str:
-        for process in process_list:
-            for cmd_arg in process['cmd']:
-                if m := re.search(rf'steamapps/common/{path_element}/(.+\.exe)', cmd_arg):
-                    return m.group(1)
-
-    def find_steam_proton_game() -> GameMetadata:
-        for process in process_list:
-            if process['name'] == 'steam-runtime-launch-client':
-                for idx,cmd_arg in enumerate(process['cmd']):
+    def find_steam_game() -> GameInfo:
+        processes = get_all_processes()
+        for process in processes:
+            if process.name == 'steam-runtime-launch-client':
+                for idx,cmd_arg in enumerate(process.args):
                     if cmd_arg =='--directory':
-                        next_cmd_arg =  process['cmd'][idx+1]
+                        next_cmd_arg =  process.args[idx+1]
                         if 'steamapps/common' in next_cmd_arg:
                             if m := re.search(r'steamapps/common/(.+)', next_cmd_arg):
                                 game_title = m.group(1)
-                                game_exe = find_steam_proton_game_executable_by_path_element(game_title)
+                                log_main.debug('game_title: %s', game_title)
 
-                                return GameMetadata(
-                                    title=game_title,
-                                    exe=game_exe,
-                                    pid=process['pid']
-                                    )
+                                if game_title:
+                                    log_main.debug('sentinel process ID: %s', process.pid)
 
-    detected_game = find_steam_proton_game()
+                                    # TODO
+                                    # This will only work if Steam client is running in a flatpak
+                                    def get_pipewire_clients() -> list[dict]:
+                                        objects = json.loads(subprocess.check_output('pw-dump'))
+                                        def is_steam_game_client_obj(obj) -> bool:
+                                            return (
+                                                    obj['type'] == 'PipeWire:Interface:Client'
+                                                and 'pipewire.access.portal.app_id' in obj['info']['props']
+                                                and obj['info']['props']['pipewire.access.portal.app_id'] == 'com.valvesoftware.Steam'
+                                                and 'application.name' in obj['info']['props']
+                                                and obj['info']['props']['application.name'] != 'Steam'
+                                                and obj['info']['props']['application.name'] != 'Steam Voice Settings'
+                                            )
+                                        return list(filter(is_steam_game_client_obj, objects))
+                                    log_main.debug('waiting for pipewire client to appear')
+                                    pipewire_application_name = None
+                                    while True:
+                                        matching_pipewire_clients = get_pipewire_clients()
+                                        if len(matching_pipewire_clients) > 0:
+                                            pipewire_application_name = matching_pipewire_clients[0]['info']['props']['application.name']
+                                            log_main.debug('pipewire client app name: %s', pipewire_application_name)
+                                            break
+                                        else:
+                                            log_main.debug('still waiting')
+                                            time.sleep(0.5)
+
+                                    return GameInfo(
+                                        title=game_title,
+                                        pipewire_name=pipewire_application_name,
+                                        pid=process.pid
+                                        )
+
+    detected_game = find_steam_game()
     if detected_game is None:
         log_main.info('game was not detected')
         return
@@ -149,7 +171,7 @@ def cycle():
 
     proc = gsr_exec(
         output_file=save_file,
-        audio_application_name=detected_game.exe
+        pipewire_name=detected_game.pipewire_name
         )
     log_main.info('GSR started, PID %d', proc.pid)
     log_main.info('watching for game process to exit')
